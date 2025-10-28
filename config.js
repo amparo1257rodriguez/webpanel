e6e2d48847856ae1774b37007887ecee1aaf290e
ChasmannJoel <joelchasmann@gmail.com>
2025-10-28 14:10:11 -0300

    Cambios recientes

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.WEBPANEL_CONFIG = {
   nombrePanel: "GOAT GAMING",
   logo: "assets/logo.png", // Cambia por la ruta del logo del cliente
   colorLetra: "#222222", // Color para letras y textos
-  colorFondo: "#00a6ffff", // Color de fondo principal
-  colorDetalle: "#dd27acb7", // Color para detalles y banner
+  colorFondo: "#909c45ff", // Color de fondo principal
+  colorDetalle: "#ffeb14ff", // Color para detalles y banner
   whatsapp: "5491123456789" // Solo números, con código de país
 };
